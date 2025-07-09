0547615f7c5746daed2b5728709ab1625c058d79
DropMasterAI <bjk021112@gmail.com>
2025-07-08 21:44:50 -0400

    Updates

--- electron.js
+++ electron.js
@@ -1,5 +1,26 @@
-"main": "public/electron.js",
-"scripts": {
-  "start:electron": "electron .",
-  "dist": "electron-builder"
+// public/electron.js
+const path            = require('path');
+const { app, BrowserWindow } = require('electron');
+
+// Start your Express server
+require(path.join(__dirname, '../server/index.js'));
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+    }
+  });
+
+  // Load your built React app
+  win.loadFile(path.join(__dirname, '../dist/index.html'));
 }
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
